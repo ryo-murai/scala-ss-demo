42ef581ce3507be9ef691d15a0192d2ea5f315d8
ryo-murai <murai.ry@gmail.com>
2018-04-10 15:01:12 +0900

    implement subtract, multply, divide of Rational class

--- src/main/scala/demo/rational.scala
+++ src/main/scala/demo/rational.scala
@@ -11,12 +11,25 @@ class Rational(n: Int, d: Int) {
     if(y == 0) x else gcd(y, x % y)
   
   private val g = gcd(n, d)
+  // numorator
   val numor: Int = n/g
+
+  // denominator
   val denom: Int = d/g
 
   def +(that: Rational) = 
     new Rational(numor * that.denom + that.numor * denom,
       denom * that.denom)
+
+  def -(that: Rational) =
+    new Rational(numor * that.denom - that.numor * denom,
+      denom * that.denom)
+
+  def *(that: Rational) =
+    new Rational(numor * that.numor, denom * that.denom)
+
+  def /(that: Rational) =
+    new Rational(numor * that.denom, denom * that.numor)
 
   override def toString: String = s"$numor / $denom"
 }
